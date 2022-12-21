a658370602e5468be84c1c34c0956cc2e3db4829
Arif6474 <arifulislam64743@gmail.com>
2022-12-21 22:53:54 +0600

    initial set up

---
+++ src/components/routes/routes.js
@@ -0,0 +1,45 @@
+import { createBrowserRouter } from "react-router-dom";
+import AboutUs from "../AboutUs/AboutUs";
+import Blog from "../Blog/Blog";
+import ContactUs from "../ContactUs/ContactUs";
+import Home from "../Home/Home";
+import Main from "../Layout/Main/Main";
+import SignUp from "../SignUp/SignUp";
+
+
+const routes = createBrowserRouter([
+{
+  path: "/",
+  element: <Main/>,
+  children : [
+    
+    {
+      path: "/",
+      element: <Home/>
+    },
+    {
+      path: "/home",
+      element: <Home/>
+    },
+    {
+      path: "/about-us",
+      element: <AboutUs/>
+    },
+    {
+      path: "/blog",
+      element: <Blog/>
+    },
+    {
+      path: "/contact-us",
+      element: <ContactUs/>
+    },
+    {
+      path: "/sign-up",
+      element: <SignUp/>
+    }
+    
+  ]
+}
+  ]);
+
+export default routes;
