e0da4044e14a61d38130065e4b3a7fc939d3d75d
Arif6474 <arifulislam64743@gmail.com>
2022-12-22 17:42:18 +0600

    design know us section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Categories from './Categories';
 import FoodDelivery from './FoodDelivery';
+import KnowUs from './KnowUs';
 
 const Home = () => {
     return (
         <div>
             <FoodDelivery/>
             <Categories/>
+            <KnowUs/>
         </div>
     );
 };
--- src/components/Home/KnowUs.js
+++ src/components/Home/KnowUs.js
@@ -1,11 +1,34 @@
-import React from 'react';
-
+import React from "react";
+import "./KnowUs.css";
+import "../Header/Header.css";
+import know from "../../components/images/know-us.jpg";
 const KnowUs = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="know-container">
+      <div>
+      <h1 className="know-title">
+        Get to know <span className="us">us</span>
+      </h1>
+      </div>
+      <div className="know-us">
+        <div className="know-img">
+          <img src={know} alt="" className="get-img"/>
         </div>
-    );
+        <div>
+        <div className="teamwork">
+      <p >
+      Teamwork <span className="success"> is the key to our sucess</span>
+      </p>
+      </div>
+          <p className="know-des">
+            At TasteNow we make sure that every decision is determined by a team
+            of us. Everyone´s opinion matters, and as group, we are stronger. We
+            firmly belive that team work, makes the dream work.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default KnowUs;
